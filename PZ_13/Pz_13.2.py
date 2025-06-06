8d00385aa7362087842d9df0efd20af896c736fb
Luuuubov <Lubovreva75@gmail.com>
2025-06-06 23:23:36 +0300

    отредактировала 13пр

--- PZ_13/Pz_13.2.py
+++ PZ_13/Pz_13.2.py
@@ -2,12 +2,12 @@
 # элементов.
 
 import random
-matrix = [[random.randint(1, 20) for _ in range(4)] for _ in range(4)]
-print(f"Сгенерированная матрица: {matrix}")
-# for row in matrix:
-#     print(row)
 
-ind = [0, 2]
-a = list(map(lambda i: sum(matrix[i]) / len(matrix[i]), ind))
-for i, avg in zip(ind, a):
+matrix = [[random.randint(1, 20) for _ in range(4)] for _ in range(4)]
+print(f"Сгенерированная матрица:")
+for i in matrix:
+   print(i)
+
+for i in [0, 2]:
+    avg = sum(matrix[i]) / len(matrix[i])
     print(f"Среднее арифметическое  нечётной строки c индексом {i}: {avg}")
--- PZ_13/Pz_13.py
+++ PZ_13/Pz_13.py
@@ -2,5 +2,8 @@
 
 import random
 matrix = [[random.randint(0, 20) for _ in range(4)] for _ in range(4)]
-print(f"Сгенерированная матрица: {matrix} ")
+print(f"Сгенерированная матрица: ")
+for i in matrix:
+   print(i)
+
 print("Минимальный элемент в предпоследнем столбце:", min(i[-2] for i in matrix))
